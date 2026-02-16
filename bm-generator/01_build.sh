f8cced5ca2ea89f07bb61434432198453c548aab
Lilith Oberhauser <lilith.oberhauser@huawei.com>
2026-02-16 11:13:40 +0100

    Refactor scripts and cmake (#18)
    Change
    
    - move *.sh from bench to helpers
    - move general formatting targets from bench/CMakeLists.txt
      to root CMakeLists.txt
    - use ls-files in clang-format.sh instead of find
    - use ls-files in license-check instead of find
    - update build directory path in all affected places
    - update syzkaller targets and target dependency
    - auto-format after generation
    - license-check is run with clang-format now
    
    Add
    
    - CmakeLists.txt to project root
    
    ---------
    
    Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

--- bm-generator/01_build.sh
+++ bm-generator/01_build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Copyright (C) Huawei Technologies Co., Ltd. 2026. All rights reserved.
+# SPDX-License-Identifier: MIT
+
 
 GOBIN="`go env GOBIN`"
 
@@ -15,10 +18,10 @@ SCRIPT_SYZ_SRC="helper/find_syzkaller_src.sh"
  : ${DIR_SYZ_SRC:=$(${SCRIPT_SYZ_SRC})}
 
 if [ ! -d "${DIR_SYZ_SRC}" ]; then
-  echo "syzkaller source dir not found. Running:"
-  echo "  cmake -S. -Bbuild"
-  cmake -S. -Bbuild
-  cmake --build build
+  echo "syzkaller source dir not found."
+  echo "  Building syzkaller with cmake ..."
+  cmake -S../ -B../build -DCSB_BM_GENERATOR=ON
+  cmake --build ../build --target syzkaller
  DIR_SYZ_SRC=$(${SCRIPT_SYZ_SRC})
   if [ ! -d "${DIR_SYZ_SRC}" ]; then
     echo "Failed setting up syzkaller sources."
--- bm-generator/05_generate.sh
+++ bm-generator/05_generate.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Copyright (C) Huawei Technologies Co., Ltd. 2026. All rights reserved.
 # SPDX-License-Identifier: MIT
-
-
-cmake -S. -Bbuild
-cmake --build build --target csb-generate
-cmake --build build --target csb-gen-agg
+cmake -S../ -B../build -DCSB_BM_GENERATOR=ON
+cmake --build ../build --target csb-generate
+cmake --build ../build --target csb-gen-agg
