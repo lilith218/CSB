0a08e823012daa3736c2ce2ce584e1f8058bcf1d
Lilith Oberhauser <lilith.oberhauser@huawei.com>
2026-06-15 16:17:09 +0300

    update
    Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

--- bm-runner/monitors/perflock.py
+++ bm-runner/monitors/perflock.py
@@ -2,9 +2,7 @@
 # SPDX-License-Identifier: MIT
 
 import os
-
 import pandas as pd
-
 from monitors.monitor import Monitor
 from utils.logger import bm_log, LogType
 from utils.process import BackgroundProcess
@@ -15,11 +13,11 @@ from bm_visualize import plot_chart, PlotConfig
 
 class PerfLock(Monitor):
     LOCK_CONTENTION_CSV = "lock-contention.csv"
-    LOCK_CONTENTION_PLOT = "lock-contention.png"
+    TARGET_METRIC = "avg_wait"
     LOCK_CONTENTION_SEPARATOR = ";"
     LOCK_CONTENTION_TOP_N = 20
-    TARGET_METRIC = "avg_wait"
 
+    # The command `perf lock contention -x ";"` will output a CSV with the following header
     # output: contended; total wait; max wait; avg wait; type; caller
     header = ["contended", "total_wait", "max_wait", "avg_wait", "type", "caller"]
 
@@ -27,7 +25,7 @@ class PerfLock(Monitor):
         super().__init__(dir=output_dir, args=args)
         self.name = "perf-lock"
         self.perf_lock_data = f"{self.name}.data"
-        self.perf_contention_csv = os.path.join(self.dir, f"{self.name}-report.csv")
+        self.perf_contention_csv = os.path.join(self.dir, self.LOCK_CONTENTION_CSV)
         cmds = [
             "sudo",
             "perf",
@@ -42,8 +40,8 @@ class PerfLock(Monitor):
             self.perf_lock_data,
         ]
         cmds.extend(args)
+
         # TODO: check if kernel flags are set
-        # TODO: check if lock contention is supported
         self.perf_lock = BackgroundProcess(
             name=self.name,
             out_dir=output_dir,
@@ -55,21 +53,28 @@ class PerfLock(Monitor):
     def start(self):
         self.perf_lock.start()
 
+    def stop(self):
+        if self.perf_lock is not None:
+            self.perf_lock.stop()
+
     def collect_results(self):
         output = ""
         if self.__run_lock_contention():
             df = read_data_frame_from_csv(self.perf_contention_csv, names=self.header)
-            self.__plot(df.head(self.LOCK_CONTENTION_TOP_N))
-            if df is None:
-                bm_log(f"{self.name} did not produce a valid data-frame", LogType.ERROR)
-                return ""
-            avg_wait = df["avg_wait"].mean()
-            max_wait      = df["max_wait"].max()
-            total_wait    = df["total_wait"].sum()
-
-            output += f"perf_lock_avg_wait={avg_wait};"
-            output += f"perf_lock_max_wait={max_wait};"
-            output += f"perf_lock_total_wait={total_wait};"
+            if df is not None:
+                # dump detailed plot of head results
+                self.__plot(df.head(self.LOCK_CONTENTION_TOP_N))
+                if df is None:
+                    bm_log(f"{self.name} did not produce a valid data-frame", LogType.ERROR)
+                    return ""
+                # summary of all locks per run
+                avg_wait = df["avg_wait"].mean()
+                max_wait = df["max_wait"].max()
+                total_wait = df["total_wait"].sum()
+                # this will be appended to the final csv
+                output += f"perf_lock_avg_wait={avg_wait};"
+                output += f"perf_lock_max_wait={max_wait};"
+                output += f"perf_lock_total_wait={total_wait};"
         return output
 
     def __run_lock_contention(self) -> bool:
@@ -78,27 +83,40 @@ class PerfLock(Monitor):
             "perf",
             "lock",
             "contention",
-            "-k",
+            "-k",  # sort by average wait
             self.TARGET_METRIC,
-            "-i",
+            "-i",  # input file is the output of `perf lock record`
             self.perf_lock_data,
-            "-x",
+            "-x",  # output report should be a CSV with `;` as delimiter
             ";",
             "--output",
             self.perf_contention_csv,
         ]
         try:
+            # perf lock contention is not available on older kernel versions
+            # the command can fail
             shell_out(command=cmd, current_dir=self.dir)
             return True
-        except Exception as e:
+        except Exception:
             bm_log("perf lock raised an error, check if it is supported.", LogType.ERROR)
             return False
 
-    def __plot(self, df:pd.DataFrame):
-        subject = "avg_wait"
-        cfg = PlotConfig(y=subject, x="caller", hue="type", shape="barplot")
-        plot_chart(cfg, df, os.path.join(self.dir, "lock-contention"))
-
-    def stop(self):
-        if self.perf_lock is not None:
-            self.perf_lock.stop()
+    def __plot(self, df: pd.DataFrame):
+        subjects = [
+            ("contended", "Contended"),
+            ("avg_wait", "Average Wait"),
+            ("total_wait", "Total Wait"),
+            ("max_wait", "Max Wait"),
+        ]
+        for y, label in subjects:
+            cfg = PlotConfig(
+                y=y,
+                y_lbl=label,
+                x="caller",
+                x_lbl="Caller Function",
+                hue="type",
+                hue_lbl="Lock Type",
+                shape="barplot",
+            )
+            plot_file = os.path.join(self.dir, f"perf_lock_{y}")
+            plot_chart(cfg, df, plot_file)
