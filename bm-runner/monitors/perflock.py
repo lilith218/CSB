c6aede21a17d58cc1c53ee7ec95f61ce4f404495
Lilith Oberhauser <lilith.oberhauser@huawei.com>
2026-06-15 13:03:45 +0300

    sort results, only care for the first 20
    Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

--- bm-runner/monitors/perflock.py
+++ bm-runner/monitors/perflock.py
@@ -18,6 +18,7 @@ class PerfLock(Monitor):
     LOCK_CONTENTION_PLOT = "lock-contention.png"
     LOCK_CONTENTION_SEPARATOR = ";"
     LOCK_CONTENTION_TOP_N = 20
+    TARGET_METRIC = "avg_wait"
 
     # output: contended; total wait; max wait; avg wait; type; caller
     header = ["contended", "total_wait", "max_wait", "avg_wait", "type", "caller"]
@@ -55,33 +56,34 @@ class PerfLock(Monitor):
         self.perf_lock.start()
 
     def collect_results(self):
-        VALUE_COL = "avg_wait"
+        VALUE_COL = self.TARGET_METRIC
         KEY_COL = "caller"
         INFO_COL = "type"
         output = ""
-        self.__run_lock_contention()
-        df = read_data_frame_from_csv(self.perf_contention_csv, names=self.header)
-        self.__plot(df)
-        print(df)
-        if df is None:
-            bm_log(f"{self.name} did not produce a valid data-frame", LogType.ERROR)
-            return ""
-        for _, row in df.iterrows():
-            value = row[VALUE_COL]
-            key = row[KEY_COL]
-            info = row[INFO_COL]
-            if pd.notna(value) and pd.api.types.is_number(value):
-                output += f"{key}_{info}={value};"
-            else:
-                bm_log(f"{self.name} could not read a valid value for {key}", LogType.ERROR)
+        if self.__run_lock_contention():
+            df = read_data_frame_from_csv(self.perf_contention_csv, names=self.header)
+            self.__plot(df.head(self.LOCK_CONTENTION_TOP_N))
+            if df is None:
+                bm_log(f"{self.name} did not produce a valid data-frame", LogType.ERROR)
+                return ""
+            for _, row in df.iterrows():
+                value = row[VALUE_COL]
+                key = row[KEY_COL]
+                info = row[INFO_COL]
+                if pd.notna(value) and pd.api.types.is_number(value):
+                    output += f"{key}_{info}={value};"
+                else:
+                    bm_log(f"{self.name} could not read a valid value for {key}", LogType.ERROR)
         return output
 
-    def __run_lock_contention(self):
+    def __run_lock_contention(self) -> bool:
         cmd = [
             "sudo",
             "perf",
             "lock",
             "contention",
+            "-k",
+            self.TARGET_METRIC,
             "-i",
             self.perf_lock_data,
             "-x",
@@ -89,10 +91,16 @@ class PerfLock(Monitor):
             "--output",
             self.perf_contention_csv,
         ]
-        shell_out(command=cmd, current_dir=self.dir)
+        try:
+            shell_out(command=cmd, current_dir=self.dir)
+            return True
+        except Exception as e:
+            bm_log("perf lock raised an error, check if it is supported.", LogType.ERROR)
+            return False
 
-    def __plot(self, df):
-        cfg = PlotConfig(y="avg_wait", x="caller", hue="type")
+    def __plot(self, df:pd.DataFrame):
+        subject = "avg_wait"
+        cfg = PlotConfig(y=subject, x="caller", hue="type")
         plot_chart(cfg, df, os.path.join(self.dir, "lock-contention"))
 
     def stop(self):
