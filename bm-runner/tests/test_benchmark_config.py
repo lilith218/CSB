0a300515ff2da334567ebfee4395f657aa81f6de
Lilith Oberhauser <lilith.oberhauser@huawei.com>
2026-07-17 16:43:28 +0300

    add license
    Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

--- bm-runner/tests/test_benchmark_config.py
+++ bm-runner/tests/test_benchmark_config.py
@@ -1,3 +1,6 @@
+# Copyright (C) Huawei Technologies Co., Ltd. 2026. All rights reserved.
+# SPDX-License-Identifier: MIT
+
 from config.benchmark import BenchmarkConfig
 from config.benchmark import MonitorType
 from monitors.perflock import PerfLock
